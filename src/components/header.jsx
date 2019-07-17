154892d3cd2a03041e6993226ea4dc08c8526438
CarlMay <info@carlmay.co.za>
2019-07-17 08:33:46 +0200

    style: styled nav and header.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {Image} from 'semantic-ui-react';
 
 const Header = () => {
+
+    const iconStyle = {
+        width: 'auto',
+        height: 'auto',
+        marginRight: '0.25rem'
+    };
+
+    const navStyle = {
+        backgroundColor: '#222',
+    };
+
+    const linkStyle = {
+        color: '#999',
+        cursor: 'pointer',
+        marginRight: '1rem',
+    };
+
     return (
-        <div className={'ui secondary pointing menu'}>
+        <div className={'ui secondary pointing menu'} style={navStyle}>
             <div className={'left menu'}>
-                <Link to={'/brainz'} className={'item'}>MusicBrainz</Link>
-                <Link to={'/last-fm'} className={'item'}>Last FM</Link>
-                <Link to={'/favorites'} className={'item'}>Favorites</Link>
+                <Link to={'/brainz'} className={'item'} style={linkStyle}>
+                    <Image src='/images/music-brainz.png' size='mini' style={iconStyle}/>
+                    <span>MusicBrainz</span>
+                </Link>
+
+                <Link to={'/last-fm'} className={'item'} style={linkStyle}>
+                    <Image src='/images/last-fm.png' size='mini' style={iconStyle}/>
+                    <span>Last FM</span>
+                </Link>
+                <Link to={'/favorites'} className={'item'} style={linkStyle}>
+                    <Image src='/images/favs.png' size='mini' style={iconStyle}/>
+                    <span>Favorites</span>
+                </Link>
             </div>
         </div>
     );
